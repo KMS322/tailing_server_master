6d5e80eccb7ffc89782414bd9d0b512ced0de57e
KMS322 <kms930322@naver.com>
2025-07-28 11:34:42 +0900

    250728_06

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
