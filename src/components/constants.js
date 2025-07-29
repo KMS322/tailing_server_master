e53b262f863b973adcedf8167a000ec900aa64f4
KMS322 <kms930322@naver.com>
2025-07-29 09:54:27 +0900

    250729_02

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3060";
-// export const API_URL = "http://211.188.55.131:3060";
+// export const API_URL = "http://localhost:3060";
+export const API_URL = "http://211.188.55.131:3060";
