666664677d08bc22853ed2199e8fabcc80712639
KMS322 <kms930322@naver.com>
2025-12-10 10:51:22 +0900

    2

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3060";
-// export const API_URL = "http://211.188.55.131:3060";
+// export const API_URL = "http://localhost:3060";
+export const API_URL = "http://98.92.20.155:3060:3060";
